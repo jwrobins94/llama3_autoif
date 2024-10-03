a59bbe9194b41186a5f05cfe24f44b783d7a4549
Joey Robinson <jwrobins94@gmail.com>
2024-10-03 14:29:44 -0700

    Initial commit

---
+++ evaluation/ifeval.py
@@ -0,0 +1,28 @@
+import torch
+from lm_eval import simple_evaluate
+from lm_eval.models.huggingface import HFLM
+from transformers import PreTrainedTokenizerFast
+import nltk
+import pandas as pd
+
+@torch.no_grad()
+def run_ifeval(model: torch.nn.Module, tokenizer: PreTrainedTokenizerFast, limit=None) -> tuple[dict[str, object], list[object]]:
+    # this download is needed for ifeval to run
+    nltk.download('punkt_tab')
+
+    model.eval()
+    result = simple_evaluate(
+        model=HFLM(pretrained=model, tokenizer=tokenizer),
+        tasks=['ifeval'],
+        device=model.device,
+        cache_requests=True,
+        log_samples=True,
+        limit=limit,
+        num_fewshot=0,
+        batch_size=1, # TODO ensure that results are correct when using larger batch sizes
+        apply_chat_template=True
+    )
+    scores = result['results']['ifeval']
+    samples = result['samples']['ifeval']
+
+    return scores, samples
---
+++ scripts/evaluate_model.py
@@ -0,0 +1,29 @@
+import lm_eval.api # for unknown reasons, this import must happen before other modules are loaded. TODO
+
+from core.model import load_model
+from core.tokenizer import load_tokenizer
+from evaluation.ifeval import run_ifeval
+import argparse
+import json
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='Script to run IFEval on a trained model')
+    parser.add_argument('--model', type=str, required=True, help='Model name, e.g. "meta-llama/Llama-3.2-1B-Instruct"')
+    parser.add_argument('--hf-api-token', type=str, required=True, help='HuggingFace API token')
+    parser.add_argument('--ckpt', type=str, default=None, help='Optional path for trained model checkpoint')
+    parser.add_argument(f'--context-length', type=int, default=2048, help='Context length')
+    parser.add_argument(f'--limit', type=int, default=None, help='Optional limit on the number of evaluation rows')
+    parser.add_argument(f'--output', type=str, default=None, help='Path to write sample results')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    tokenizer = load_tokenizer(args.hf_api_token)
+    model = load_model(args.model, tokenizer, args.context_length, args.hf_api_token) # TODO add support for state_dict
+    scores, samples = run_ifeval(model, tokenizer, args.limit or None)
+    print(scores)
+
+    if args.output:
+        with open(args.output, 'w') as f:
+            f.write(json.dumps(samples, indent=2))
