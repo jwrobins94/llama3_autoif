3bcbf411630da9fd5721ef14031fe7b20b2ca55e
Joey Robinson <jwrobins94@gmail.com>
2024-10-03 14:41:36 -0700

    batch size 1

--- evaluation/ifeval.py
+++ evaluation/ifeval.py
@@ -12,14 +12,14 @@ def run_ifeval(model: torch.nn.Module, tokenizer: PreTrainedTokenizerFast, limit
 
     model.eval()
     result = simple_evaluate(
-        model=HFLM(pretrained=model, tokenizer=tokenizer, batch_size=10),
+        model=HFLM(pretrained=model, tokenizer=tokenizer, batch_size=1),
         tasks=['ifeval'],
         device=model.device,
         cache_requests=True,
         log_samples=True,
         limit=limit,
         num_fewshot=0,
-        batch_size=10, # TODO ensure that results are correct when using larger batch sizes
+        batch_size=1, # TODO ensure that results are correct when using larger batch sizes
         apply_chat_template=True
     )
     scores = result['results']['ifeval']
