4ff319274830915df8e000d361e429ecd190ab08
Joey Robinson <jwrobins94@gmail.com>
2024-10-04 14:07:35 -0700

    more logging

--- scripts/sort_completions.py
+++ scripts/sort_completions.py
@@ -41,6 +41,7 @@ if __name__ == '__main__':
                     ok = False
                 if ok:
                     num_passes[completion_idx] += 1
+        print(num_passes)
         
         chosen = []
         rejected = []
