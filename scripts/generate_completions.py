0b774b1f5d23adb0c9eb0c63146a4ddea7e6e1a5
Joey Robinson <jwrobins94@gmail.com>
2024-10-04 11:04:24 -0700

    function to generate queries

--- scripts/generate_completions.py
+++ scripts/generate_completions.py
@@ -4,6 +4,8 @@ import argparse
 import torch
 import json
 from core.inference_utils import generate_completions, wrap_with_deepspeed_inference
+from datasets import load_dataset
+
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Script to generate completions for each instruction')
@@ -21,6 +23,31 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+def extract_query(row) -> dict[str, str]:
+    conversations = row['conversations']
+    if not conversations:
+        return {'query': ''}
+    value = conversations[0]['value']
+    if not value:
+        return {'query': ''}
+    return {'query': value}
+
+def load_queries_dataset() -> list[str]:
+    # TODO allow picking the dataset from command line
+    data = load_dataset('anon8231489123/ShareGPT_Vicuna_unfiltered', data_files='ShareGPT_V3_unfiltered_cleaned_split_no_imsorry.json')
+    data_train = data['train']
+    data_train = data_train.map(
+        extract_query,
+        batched=False,
+        remove_columns=data_train.column_names,
+        load_from_cache_file=False # the cache creates a lot of problems when debugging
+    ).filter(lambda row : 5 <= len(row['query']) <= 200)
+
+    res = []
+    for row in data_train:
+        res.append(row['query'])
+    return res
+    
 def construction_generation_prompt(query: str, instruction: str) -> str:
     # this prompt is derived from the one in the paper
     # TODO!!!!!!!!!!!!!!!!!! Replace this with actual queries.
