f75723e976306f98587421f69d8a8fd68ab2ad48
Joey Robinson <jwrobins94@gmail.com>
2024-10-08 12:58:26 -0700

    add merge script

--- scripts/merge_files.py
+++ scripts/merge_files.py
@@ -3,10 +3,9 @@ from core.data_utils import merge_outputs
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Script to merge shared input files into a single output file')
-    parser.add_argument(f'--input', type=str, required=True, help='Prefix to sharded input files')
-    parser.add_argument(f'--output', type=str, required=True, help='Output path')
+    parser.add_argument(f'--prefix', type=str, required=True, help='Prefix to sharded input files')
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_args()
-    merge_outputs(args.output)
+    merge_outputs(args.prefix)
