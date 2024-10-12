53798fe4c8d0c32a99c1e84f37caacf251902c6a
jwrobins94 <55998592+jwrobins94@users.noreply.github.com>
2024-10-12 15:20:54 -0700

    Better verification stats (#19)
    * Better verification stats
    
    * clean up
    
    * clean up

--- scripts/3_filter_instructions.py
+++ scripts/3_filter_instructions.py
@@ -19,7 +19,18 @@ def can_compile(function_str: str) -> tuple[Optional[Callable[[str], bool]], boo
         return local_evaluate, True # evaluate is generated dynamically from exec
     except:
         return None, False
-    
+
+def validate_testcase(test: dict[str, object]) -> bool:
+    if ('response' not in test) or ('result' not in test):
+        return False
+    response = test['response']
+    if not isinstance(response, str):
+        return False
+    result = test['result']
+    if not isinstance(result, bool):
+        return False
+    return True
+
 def parse_test_cases(test_group_strs: list[str]) -> list[tuple[str, bool]]:
     res = []
     for group_str in test_group_strs:
@@ -28,18 +39,14 @@ def parse_test_cases(test_group_strs: list[str]) -> list[tuple[str, bool]]:
         for test_str in test_strs:
             try:
                 test = json.loads(test_str)
-                if ('response' not in test) or ('result' not in test):
-                    continue
-                response = test['response']
-                if not isinstance(response, str):
-                    continue
-                result = test['result']
-                if not isinstance(result, bool):
+                if not validate_testcase(test):
+                    print(f'Bad test case: {test_str}')
                     continue
             except:
                 # model produced invalid json; skip it
+                print(f'Bad test case: {test_str}')
                 continue
-            res.append((response, result))
+            res.append((test['response'], test['result']))
 
     return res
 
@@ -51,6 +58,11 @@ def passes(verifier_fn: Callable[[str], bool], input_str: str, result: bool) -> 
 
 
 def passes_validation(query: str, instruction: str, verifiers: list[str], testcases: list[str]) -> tuple[dict[str, object], bool]:
+    stats = {
+        'verifier_count': len(verifiers),
+        'testcase_str_count': len(testcases)
+    }
+   
     # First filter the verifiers to those that compile; # TODO is that how the paper does it?
     filtered_verifiers = []
     verifier_functions = []
@@ -59,18 +71,22 @@ def passes_validation(query: str, instruction: str, verifiers: list[str], testca
         if ok:
             filtered_verifiers.append(verifier)
             verifier_functions.append(f)
+        else:
+            print(verifier)
     verifiers = filtered_verifiers
     print(f'Verifiers compile: {len(verifiers)}')
+    stats['compiled_verifiers'] = len(verifiers)
     if not verifiers:
-        return {}, False
+        return stats, False
     
     print(verifier_functions)
 
     # parse test cases into tuples
     test_cases = parse_test_cases(testcases) # TODO use better naming for test_cases vs testcases
     print(f'Test cases parse: {len(test_cases)}')
+    stats['test_cases_parse'] = len(test_cases)
     if not test_cases:
-        return {}, False
+        return stats, False
         
     # remove test cases that don't pass at least one verifier
     filtered_test_cases = []
@@ -85,11 +101,13 @@ def passes_validation(query: str, instruction: str, verifiers: list[str], testca
     test_cases = filtered_test_cases
     num_test_cases = len(test_cases)
     print(f'Test cases pass at least one verifier: {num_test_cases}')
+    stats['test_cases_pass_gte1'] = num_test_cases
     if not num_test_cases:
-        return {}, False
+        return stats, False
     
     # filter verifiers to those that pass at least 80% of test cases
     filtered_verifiers = []
+    total_passes = 0
     for verifier, verifier_fn in zip(verifiers, verifier_functions):
         num_passed = 0
         for input_str, result in test_cases:
@@ -98,18 +116,23 @@ def passes_validation(query: str, instruction: str, verifiers: list[str], testca
         pass_rate = num_passed / num_test_cases
         if pass_rate >= 0.8:
             filtered_verifiers.append(verifier)
+        total_passes += num_passed
     verifiers = filtered_verifiers
     print(f'Verifiers pass at least 80% of test cases: {len(verifiers)}')
+    stats['verifiers_pass_80pct'] = len(verifiers)
+    stats['total_passes'] = total_passes
     if not verifiers:
-        return {}, False
+        return stats, False
 
     # TODO note!!! Verifiers and test cases have been parsed in the returned object
-    return {
+    stats.update({
         'query': query,
         'instruction': instruction,
         'test_cases': test_cases,
         'verifiers': verifiers,
-    }, True
+        'unique_instances': 1
+    })
+    return stats, True
         
 
 if __name__ == '__main__':
@@ -122,10 +145,27 @@ if __name__ == '__main__':
     with open(args.input) as f:
         orig_instances = [json.loads(line) for line in f.read().splitlines()]
     
+    stats = {
+        'verifier_count': 0,
+        'testcase_str_count': 0,
+        'compiled_verifiers': 0,
+        'test_cases_parse': 0,
+        'test_cases_pass_gte1': 0,
+        'verifiers_pass_80pct': 0,
+        'total_passes': 0,
+        'unique_instances': 0
+    }
+
     with ProcessPoolExecutor(16) as executor:
         futures = []
         for instance in orig_instances:
-            future = executor.submit(passes_validation, **instance)
+            future = executor.submit(
+                passes_validation,
+                instance['query'],
+                instance['instruction'],
+                instance['verifiers'],
+                instance['testcases']
+            )
             futures.append(future)
         
         filtered_instances = []
@@ -138,6 +178,8 @@ if __name__ == '__main__':
             filtered_instance, ok = future.result()
             if ok:
                 filtered_instances.append(filtered_instance)
+            for k in stats:
+                stats[k] += filtered_instance.get(k, 0)
         print('Writing output.')
 
         with open(args.output, 'w') as output_file:
@@ -149,5 +191,7 @@ if __name__ == '__main__':
         for proc in multiprocessing.active_children():
             print(f'Killing stalled process: {proc.pid}')
             os.kill(proc.pid, signal.SIGTERM)
+    
+        print(stats)
     
     
